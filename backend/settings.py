09bc1da0737a7a931b7b08065fd72f06519c98b5
djangolgbtqweb <humphreynyabuto2@gmail.com>
2025-04-29 15:18:32 +0300

    SQLITEDisk

--- backend/settings.py
+++ backend/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SQLITE_PATH = os.environ.get("SQLITE_PATH", BASE_DIR / "db.sqlite3")
 
 
 # Quick-start development settings - unsuitable for production
@@ -102,7 +103,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': SQLITE_PATH,
     }
 }
 
